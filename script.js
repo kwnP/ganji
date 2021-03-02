71ad7771100d3c67aa5132dfdaccec130f6a880b
kwnP <40490945+kwnP@users.noreply.github.com>
2021-03-02 20:06:47 +0900

    파비콘 추가
    배경색 따라서 파비콘 색도 바뀝니다.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function ganji(a) {
   var year = a.value;
   if(year == 0 || year == ""){
+    document.querySelector("link[rel*='icon']").href = "ico_white.svg";
     document.getElementById('sans').innerText = "결과가 여기 나옵니다";
     document.body.style.backgroundColor = "#ffffff";
     document.body.style.color = "#000000";
@@ -14,10 +15,13 @@ function ganji(a) {
       var hanji = ["申", "酉", "戌", "亥", "子", "丑", "寅", "卯", "辰", "巳", "午", "未"];
       var sibiji = ["원숭이", "닭", "개", "돼지", "쥐", "소", "호랑이", "토끼", "용", "뱀", "말", "양"];
       var color = ["하얀", "하얀", "검은", "검은", "푸른", "푸른", "붉은", "붉은", "황금", "황금"];
+      var coloreng = ["white", "white", "black", "black", "blue", "blue", "red", "red", "yellow", "yellow"];
       var colorhex = ["#ffffff", "#ffffff", "#111133", "#111133", "#1155bb", "#1155bb", "#bb1133", "#bb1133", "#ffdd33", "#ffdd33"];
       var textcolorhex = ["#000000", "#000000", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#000000", "#000000"];
       var ganji1 = year%10;
       var ganji2 = year%12;
+
+      document.querySelector("link[rel*='icon']").href = "ico_" + coloreng[ganji1] + ".svg";
       document.getElementById('sans').innerText = year + "년은 " + cheongan[ganji1] + jiji[ganji2] + "(" + hanjagan[ganji1] + hanji[ganji2] + ")년이며, " + color[ganji1] + " " + sibiji[ganji2] + "의 해입니다.";
       document.body.style.backgroundColor = colorhex[ganji1];
       document.body.style.color = textcolorhex[ganji1];
@@ -30,6 +34,7 @@ function ganji(a) {
       var hanji = ["申", "未", "午", "巳", "辰", "卯", "寅", "丑", "子", "亥", "戌", "酉"];
       var sibiji = ["원숭이", "양", "말", "뱀", "용", "토끼", "호랑이", "소", "쥐", "돼지", "개", "닭"];
       var color = ["하얀", "황금", "황금", "붉은", "붉은", "푸른", "푸른", "검은", "검은", "하얀"];
+      var coloreng = ["white", "yellow", "yellow", "red", "red", "blue", "blue", "black", "black", "white"];
       var colorhex = ["#ffffff", "#ffdd33", "#ffdd33", "#bb1133", "#bb1133", "#1155bb", "#1155bb", "#111133", "#111133", "#ffffff"];
       var textcolorhex = ["#000000", "#000000", "#000000", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#ffffff", "#000000"];
       var ganji1 = Math.abs(year)%10 - 1;
@@ -42,6 +47,7 @@ function ganji(a) {
         ganji2 = ganji2 + 12;
       }
 
+      document.querySelector("link[rel*='icon']").href = "ico_" + coloreng[ganji1] + ".svg";
       document.getElementById('sans').innerText = year + "년은 " + cheongan[ganji1] + jiji[ganji2] + "(" + hanjagan[ganji1] + hanji[ganji2] + ")년이며, " + color[ganji1] + " " + sibiji[ganji2] + "의 해입니다.";
       document.body.style.backgroundColor = colorhex[ganji1];
       document.body.style.color = textcolorhex[ganji1];
